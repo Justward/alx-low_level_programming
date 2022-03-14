066895f480df669ca43e206b59f08a9e121f0234
justward <justiceedward3@gmail.com>
2022-03-14 11:53:35 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
-}
+
+/**
+ * *main -
+ *
+ * *Return: Always 0
+ * */
+
+
+
+ ​int​ ​main​(​void​) 
+	 ​{ 
+		  ​        ​puts​(​"​\"​Programming is like building a multilingual puzzle​"​); 
+		   ​        ​return​ (​0​); 
+		    ​}
