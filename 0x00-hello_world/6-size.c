d47ff0691ba8111217d0738d64cb91b828c5f759
justward <justiceedward3@gmail.com>
2022-03-15 11:38:44 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 
 	/** sizeof evaluates the size of a variable**/
 
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of long int: %lu byte(s)\n", sizeof(longintType));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(doubleType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
