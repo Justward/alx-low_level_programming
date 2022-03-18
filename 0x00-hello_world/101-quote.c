69abd9b868efdbb9bf5e330a230c3aac993eeb6c
Justward <justiceedward3@gmail.com>
2022-03-18 18:03:03 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
 *return: Always **/
 int main(void)
 {
-	fprintf(
-	stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
