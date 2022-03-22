58bde73a645f5a641237c027b0547f721549a219
justward <justiceedward3@gmail.com>
2022-03-22 11:50:58 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+i#include <stdio.h>
 /**
 *main - Entry point
 *return: Always **/
 int main(void)
 {
-	fprintf(
+	_putchar(
 	stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
